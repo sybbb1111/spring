cf4860e0b0820b661b29ce344e2ce9142d45cf0e
Minami <dft2007@naver.com>
2023-05-25 15:52:30 +0900

    05.25 파일업로드

--- src/main/java/com/green/board7/fileupload/FileuploadController.java
+++ src/main/java/com/green/board7/fileupload/FileuploadController.java
@@ -1,10 +1,16 @@
 package com.green.board7.fileupload;
 
 
+import com.green.board7.fileupload.model.FileEntity;
+import com.green.board7.fileupload.model.FileLoadDto;
+import com.green.board7.fileupload.model.FileuploadInsDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,9 +26,30 @@ public class FileuploadController {
         this.service = service;
     }
 
+
+
+
+    @GetMapping
+    public ResponseEntity<Resource> download(FileLoadDto dto) {
+        Resource file = (Resource) service.fileLoad(dto);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment; filename=\"" + file.getFilename() + "\"")
+
+                .body(file);
+    }
+
+
+
+
+
     @PostMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
-    public void fileupload(@RequestPart MultipartFile img){
+    public void fileupload(@RequestPart FileuploadInsDto dto
+                          , @RequestPart MultipartFile img){
+        LOGGER.info("dto" + dto);
         LOGGER.info("imgFileName" + img.getName());
+        service.fileUpload(dto, img);
+
+
     }
 }
-//언니 내가 좋아하는거 알지 언니 자리 키보드 뭔가 촥촥감겨
--- src/main/java/com/green/board7/fileupload/FileuploadService.java
+++ src/main/java/com/green/board7/fileupload/FileuploadService.java
@@ -1,7 +1,78 @@
 package com.green.board7.fileupload;
 
+import ch.qos.logback.core.rolling.SizeAndTimeBasedRollingPolicy;
+import com.green.board7.fileupload.model.FileEntity;
+import com.green.board7.fileupload.model.FileLoadDto;
+import com.green.board7.fileupload.model.FileuploadInsDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.UUID;
 
 @Service
 public class FileuploadService {
+    private FileuploadMapper mapper;
+
+    @Autowired
+    public FileuploadService(FileuploadMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    public Resource fileLoad(FileLoadDto dto) {
+        FileEntity entity = mapper.selFileById(dto);
+        try {
+            File file = new File(fileDir + entity.getPath());
+            Resource resource = new UrlResource(file.toURI());
+            if(resource.exists()) {
+                return resource;
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    @Value("${file.dir}")
+    private String fileDir;
+
+
+    public void fileUpload(FileuploadInsDto dto, MultipartFile img) {
+        System.out.println("fileDir : " + fileDir);
+
+        //원래 파일 이름 추출
+        String originFileName = img.getOriginalFilename();
+
+        //파일 이름으로 사용할 uuid 생성
+        //네트워크상에서 고유성을 보장하는 id를 만들기 위한 표준 규약
+        String uuid = UUID.randomUUID().toString();
+
+        int dotIdx = originFileName.lastIndexOf(".");
+        String ext = originFileName.substring(dotIdx);
+
+        String savedFileName = uuid + ext;
+        String savedFilePath = fileDir + savedFileName;
+
+        File file = new File(savedFilePath);
+        try {
+            img.transferTo(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        FileEntity entity = FileEntity.builder()
+                .path(savedFileName)
+                .uploader(dto.getUploader())
+                .levelValue(dto.getLevelValue())
+                .build();
+
+        mapper.insFile(entity);
+
+    }
 }
